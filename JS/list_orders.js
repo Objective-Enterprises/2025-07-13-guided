3775d07143924f20bd0d86f2d102ea839ca8176a
David Y. Stephenson <david@davidystephenson.com>
2025-07-13 12:51:30 -0400

    Display order table

---
+++ JS/list_orders.js
@@ -0,0 +1,22 @@
+const tbody = document.querySelector('tbody')
+
+async function download () {
+  const response = await fetch('http://localhost:3000/orders')
+  const data = await response.json()
+  console.log('data', data)
+  data.forEach(order => {
+    const row = `
+      <tr>
+        <td>${order.id}</td>
+        <td>${order.date}</td>
+        <td>${order.name}</td>
+        <td>${order.price}</td>
+        <td>${order.paymentStatus}</td>
+        <td>${order.orderStatus}</td>
+        <td></td>
+      </tr>
+    `
+    tbody.innerHTML += row
+  })
+}
+download()
